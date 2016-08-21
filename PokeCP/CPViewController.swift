531d9a0dcf429ecde7ed6f5a3f346fad79c3d3be
Liguo Jiao's Mac <jiaoliguo888@gmail.com>
2016-08-21 17:01:08 +1200

    commit for save

---
+++ PokeCP/CPViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CPViewController.swift
+//  PokeCP
+//
+//  Created by Liguo Jiao on 16/8/21.
+//  Copyright © 2016年 Liguo Jiao. All rights reserved.
+//
+
+import UIKit
+
+class CPViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+    }
+}
---
+++ PokeCP/Model/Pokemon.swift
@@ -0,0 +1,13 @@
+//
+//  Pokemon.swift
+//  PokeCP
+//
+//  Created by Liguo Jiao on 16/8/21.
+//  Copyright © 2016年 Liguo Jiao. All rights reserved.
+//
+
+import Foundation
+
+struct pokemon {
+    <#fields#>
+}
