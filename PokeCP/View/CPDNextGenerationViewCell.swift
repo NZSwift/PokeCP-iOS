6cff5773eb9c7c4b998525e64c44dc637d97e5c7
Liguo Jiao's Mac <jiaoliguo888@gmail.com>
2016-09-05 21:47:57 +1200

    calculation

---
+++ PokeCP/View/CPDNextGenerationViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CPDNextGenerationViewCell.swift
+//  PokeCP
+//
+//  Created by Liguo Jiao on 16/9/5.
+//  Copyright © 2016年 Liguo Jiao. All rights reserved.
+//
+
+import UIKit
+
+class CPDNextGenerationViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
